9daa0e32830c098683165a87ffec7499073326c1
NoobaNyte <lukecopa123@gmail.com>
2026-06-24 17:36:52 -0600

    more wall tweaks

--- Scenes/RotatingPlatform/generate_piece.gd
+++ Scenes/RotatingPlatform/generate_piece.gd
@@ -135,6 +135,9 @@ func generate_wall(piece: Node3D):
 			child.rotation.z = deg_to_rad(-90)
 			child.position.z = -0.75
 
+			var wall_collision = child.get_node("CollisionShape3D")
+			wall_collision.reparent(selectedwall)
+
 
 func _on_destroy_detection_area_area_entered(area: Area3D) -> void:
 	if area.name == "DestroyDetectionArea":
