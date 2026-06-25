1b926b58f903cc0e6408575a9fdce31e6550446f
NoobaNyte <lukecopa123@gmail.com>
2026-06-25 13:07:32 -0600

    improved destroying falling objects

--- Scenes/Rotating Platform/Falling Objects/BaseFallingObject.gd
+++ Scenes/Rotating Platform/Falling Objects/BaseFallingObject.gd
@@ -2,3 +2,13 @@ extends RigidBody3D
 class_name BaseFallingObject
 
 var going_to_be_destroyed: bool = false
+
+func _ready() -> void:
+	PlayerGlobals.reset_game.connect(destroy_after_cooldown)
+
+func destroy_after_cooldown():
+	if not going_to_be_destroyed:
+		print("queing for destroy")
+		going_to_be_destroyed = true
+		await get_tree().create_timer(2).timeout # wait for object to get offscreen a bit
+		queue_free()
--- Scenes/Rotating Platform/game_over_area.gd
+++ Scenes/Rotating Platform/game_over_area.gd
@@ -5,8 +5,4 @@ func _on_body_entered(body: Node3D) -> void:
 		PlayerGlobals.game_over.emit()
 	
 	if body.is_in_group("FallingObject"):
-		if body.is_class("BaseFallingObject"):
-			if not body.going_to_be_destroyed:
-				body.going_to_be_destroyed = true
-				await get_tree().create_timer(2).timeout
-				body.queue_free()
+		body.destroy_after_cooldown()
