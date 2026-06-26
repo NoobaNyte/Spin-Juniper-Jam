6afc2e1ca14456da91202ec58d7a62abe254761e
NoobaNyte <lukecopa123@gmail.com>
2026-06-25 22:03:43 -0600

    added footstep sfx but not hooked up to player yet

--- Scenes/Main/audio.gd
+++ Scenes/Main/audio.gd
@@ -1,9 +1,11 @@
 extends Node
 
 var all_spinner_ticks_sfx: Node
+var all_footstep_sfx: Node
 
 func _ready() -> void:
 	all_spinner_ticks_sfx = find_child("SpinnerTicks", true, false)
+	all_footstep_sfx = find_child("Footsteps", true, false)
 
 
 func play_random_wheel_tick_sfx() -> void:
@@ -22,4 +24,12 @@ func play_poof_sfx():
 func play_purchase_sfx():
 	var sfx = find_child("Purchase", true, false)
 	#sfx.pitch_scale = randf_range(1.0, 1.1)
+	sfx.play()
+
+func play_random_footstep_sfx():
+	var children = all_footstep_sfx.get_children()
+	if children.is_empty():
+		return
+	
+	var sfx: AudioStreamPlayer = children[randi() % children.size()]
 	sfx.play()
