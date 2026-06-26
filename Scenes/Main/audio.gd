9d56b99050f6f425bfce5dbabe62017323cac184
NoobaNyte <lukecopa123@gmail.com>
2026-06-25 21:24:24 -0600

    implemented tick sfx when wheel spins past 200 speed

--- Scenes/Main/audio.gd
+++ Scenes/Main/audio.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	all_spinner_ticks_sfx = find_child("SpinnerTicks", true, false)
 
 
-func play_random_wheel_tick_sfx():
+func play_random_wheel_tick_sfx() -> void:
 	var children = all_spinner_ticks_sfx.get_children()
 	if children.is_empty():
 		return
