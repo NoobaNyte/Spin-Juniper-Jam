86fab6a2487c6c7165198593b14d6e25470612ab
NoobaNyte <lukecopa123@gmail.com>
2026-06-26 20:49:08 -0600

    updated level label names

--- Scenes/Main/level_label.gd
+++ Scenes/Main/level_label.gd
@@ -10,10 +10,25 @@ func _ready() -> void:
 	update_text()
 
 func update_text():
-	level_text.text = "LEVEL " + str(PlayerGlobals.selected_level)
 	match PlayerGlobals.selected_level:
-		1: reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_1_jackpot_amount)
-		2: reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_2_jackpot_amount)
-		3: reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_3_jackpot_amount)
-		4: reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_4_jackpot_amount)
-		5: reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_5_jackpot_amount)
+		# default font size: 193
+		1:
+			level_text.text = "Carnival Fun"
+			level_text.add_theme_font_size_override("normal_font_size", 135)
+			reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_1_jackpot_amount)
+		2:
+			level_text.text = "Carnivorous Fun"
+			level_text.add_theme_font_size_override("normal_font_size", 105)
+			reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_2_jackpot_amount)
+		3:
+			level_text.text = "Wave Ride"
+			level_text.add_theme_font_size_override("normal_font_size", 150)
+			reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_3_jackpot_amount)
+		4:
+			level_text.text = "Midnight Festival"
+			level_text.add_theme_font_size_override("normal_font_size", 100)
+			reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_4_jackpot_amount)
+		5:
+			level_text.text = "Flammable"
+			level_text.add_theme_font_size_override("normal_font_size", 145)
+			reward_text.text = "+[img=150x0]res://Assets/2D/Ticket Black Stroke.png[/img] " + str(WheelGlobals.level_5_jackpot_amount)
